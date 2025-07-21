52ad83fa002bedab223db1b3a85e537a57d5afc0
Kyle Mwendwa <kklegendofgaming@gmail.com>
2025-07-21 11:46:43 +0300

    register added to context

---
+++ src/contexts/AuthContext.jsx
@@ -0,0 +1,38 @@
+import React, { createContext, useContext, useState } from "react";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [token, setToken] = useState(localStorage.getItem("token") || null);
+  // add the render links to the fetch functions for both register and login
+  const register = async (email, password) => {
+    const response = await fetch("", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
+    if (!response.ok) {
+      throw new Error("Registration");
+    }
+    const data = await response.json();
+    const token = data.token;
+    setToken(token);
+    localStorage.setItem("token", token);
+    return token;
+  };
+
+  const login = async (email, password) => {};
+  const logout = () => {
+    setToken = null;
+    localStorage.removeItem("token");
+  };
+
+  return (
+    <AuthContext.Provider value={{ token, register, login, logout }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+export const useAuth = () => useContext(AuthContext);
