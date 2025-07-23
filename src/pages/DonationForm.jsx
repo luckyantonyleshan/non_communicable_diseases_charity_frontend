fcb57defdc5ef92418443f45bcd1da34732f6a1b
LYDIA <lydiaayitsoh@gmail.com>
2025-07-23 11:50:53 +0300

    changes on danationform and added some styling

--- src/pages/DonationForm.jsx
+++ src/pages/DonationForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import "./App.css";
 function DonationForm() {
   const [username, setUsername] = useState('');
   const [email, setEmail] = useState('');
